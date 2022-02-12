205a820a116aabd7d1a026987ffa60948f9b41b2
Md. Zawad Hossain Rifat <zawadhossain7@gmail.com>
2022-02-12 19:50:48 +0600

    commit1

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace RHFactory_CustomUi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rH_DatePicker1 = new RHFactory_CustomUi.RH_DatePicker();
+            this.rH_CircularPictureBox1 = new RHFactory_CustomUi.RH_CircularPictureBox();
+            this.rH_ComboBox1 = new RHFactory_CustomUi.RH_ComboBox();
+            this.rH_RadioButton1 = new RHFactory_CustomUi.RH_RadioButton();
+            this.rH_ProgressBar1 = new RHFactory_CustomUi.RH_ProgressBar();
+            this.rH_ToggleButton1 = new RHFactory_CustomUi.RH_ToggleButton();
+            this.rH_Button1 = new RHFactory_CustomUi.RH_Button();
+            this.rH_TextBox1 = new RHFactory_CustomUi.RH_TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rH_CircularPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rH_DatePicker1
+            // 
+            this.rH_DatePicker1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rH_DatePicker1.BorderSize = 0;
+            this.rH_DatePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.rH_DatePicker1.Location = new System.Drawing.Point(111, 111);
+            this.rH_DatePicker1.MinimumSize = new System.Drawing.Size(4, 35);
+            this.rH_DatePicker1.Name = "rH_DatePicker1";
+            this.rH_DatePicker1.Size = new System.Drawing.Size(225, 35);
+            this.rH_DatePicker1.SkinColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_DatePicker1.TabIndex = 6;
+            this.rH_DatePicker1.TextColor = System.Drawing.Color.White;
+            // 
+            // rH_CircularPictureBox1
+            // 
+            this.rH_CircularPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.rH_CircularPictureBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.rH_CircularPictureBox1.BorderColor2 = System.Drawing.Color.HotPink;
+            this.rH_CircularPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.rH_CircularPictureBox1.BorderSize = 2;
+            this.rH_CircularPictureBox1.GradientAngle = 50F;
+            this.rH_CircularPictureBox1.Location = new System.Drawing.Point(410, 111);
+            this.rH_CircularPictureBox1.Name = "rH_CircularPictureBox1";
+            this.rH_CircularPictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.rH_CircularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rH_CircularPictureBox1.TabIndex = 5;
+            this.rH_CircularPictureBox1.TabStop = false;
+            // 
+            // rH_ComboBox1
+            // 
+            this.rH_ComboBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rH_ComboBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_ComboBox1.BorderSize = 1;
+            this.rH_ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.rH_ComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.rH_ComboBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.rH_ComboBox1.IconColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_ComboBox1.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.rH_ComboBox1.ListTextColor = System.Drawing.Color.DimGray;
+            this.rH_ComboBox1.Location = new System.Drawing.Point(72, 54);
+            this.rH_ComboBox1.MinimumSize = new System.Drawing.Size(200, 30);
+            this.rH_ComboBox1.Name = "rH_ComboBox1";
+            this.rH_ComboBox1.Padding = new System.Windows.Forms.Padding(1);
+            this.rH_ComboBox1.Size = new System.Drawing.Size(200, 30);
+            this.rH_ComboBox1.TabIndex = 4;
+            this.rH_ComboBox1.Texts = "";
+            // 
+            // rH_RadioButton1
+            // 
+            this.rH_RadioButton1.AutoSize = true;
+            this.rH_RadioButton1.Checked = true;
+            this.rH_RadioButton1.CheckedColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_RadioButton1.Location = new System.Drawing.Point(368, 84);
+            this.rH_RadioButton1.MinimumSize = new System.Drawing.Size(0, 21);
+            this.rH_RadioButton1.Name = "rH_RadioButton1";
+            this.rH_RadioButton1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.rH_RadioButton1.Size = new System.Drawing.Size(117, 21);
+            this.rH_RadioButton1.TabIndex = 3;
+            this.rH_RadioButton1.TabStop = true;
+            this.rH_RadioButton1.Text = "rH_RadioButton1";
+            this.rH_RadioButton1.UnCheckedColor = System.Drawing.Color.Gray;
+            this.rH_RadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // rH_ProgressBar1
+            // 
+            this.rH_ProgressBar1.ChannelColor = System.Drawing.Color.LightSteelBlue;
+            this.rH_ProgressBar1.ChannelHeight = 6;
+            this.rH_ProgressBar1.ForeBackColor = System.Drawing.Color.RoyalBlue;
+            this.rH_ProgressBar1.ForeColor = System.Drawing.Color.White;
+            this.rH_ProgressBar1.Location = new System.Drawing.Point(123, 12);
+            this.rH_ProgressBar1.Name = "rH_ProgressBar1";
+            this.rH_ProgressBar1.ShowMaximun = false;
+            this.rH_ProgressBar1.ShowValue = RHFactory_CustomUi.TextPosition.Right;
+            this.rH_ProgressBar1.Size = new System.Drawing.Size(100, 23);
+            this.rH_ProgressBar1.SliderColor = System.Drawing.Color.RoyalBlue;
+            this.rH_ProgressBar1.SliderHeight = 6;
+            this.rH_ProgressBar1.SymbolAfter = "";
+            this.rH_ProgressBar1.SymbolBefore = "";
+            this.rH_ProgressBar1.TabIndex = 2;
+            this.rH_ProgressBar1.Value = 40;
+            // 
+            // rH_ToggleButton1
+            // 
+            this.rH_ToggleButton1.AutoSize = true;
+            this.rH_ToggleButton1.Checked = true;
+            this.rH_ToggleButton1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rH_ToggleButton1.Location = new System.Drawing.Point(349, 33);
+            this.rH_ToggleButton1.MinimumSize = new System.Drawing.Size(45, 22);
+            this.rH_ToggleButton1.Name = "rH_ToggleButton1";
+            this.rH_ToggleButton1.OffBackColor = System.Drawing.Color.Gray;
+            this.rH_ToggleButton1.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.rH_ToggleButton1.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_ToggleButton1.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+            this.rH_ToggleButton1.Size = new System.Drawing.Size(45, 22);
+            this.rH_ToggleButton1.TabIndex = 1;
+            this.rH_ToggleButton1.UseVisualStyleBackColor = true;
+            // 
+            // rH_Button1
+            // 
+            this.rH_Button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_Button1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_Button1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rH_Button1.BorderRadius = 20;
+            this.rH_Button1.BorderSize = 0;
+            this.rH_Button1.FlatAppearance.BorderSize = 0;
+            this.rH_Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rH_Button1.ForeColor = System.Drawing.Color.White;
+            this.rH_Button1.Location = new System.Drawing.Point(151, 232);
+            this.rH_Button1.Name = "rH_Button1";
+            this.rH_Button1.Size = new System.Drawing.Size(150, 40);
+            this.rH_Button1.TabIndex = 0;
+            this.rH_Button1.Text = "rH_Button1";
+            this.rH_Button1.TextColor = System.Drawing.Color.White;
+            this.rH_Button1.UseVisualStyleBackColor = false;
+            // 
+            // rH_TextBox1
+            // 
+            this.rH_TextBox1.AutoSize = true;
+            this.rH_TextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.rH_TextBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.rH_TextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.rH_TextBox1.BorderRadius = 6;
+            this.rH_TextBox1.BorderSize = 1;
+            this.rH_TextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rH_TextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.rH_TextBox1.Location = new System.Drawing.Point(123, 180);
+            this.rH_TextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.rH_TextBox1.Multiline = true;
+            this.rH_TextBox1.Name = "rH_TextBox1";
+            this.rH_TextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.rH_TextBox1.PasswordChar = true;
+            this.rH_TextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.rH_TextBox1.PlaceholderText = "your text";
+            this.rH_TextBox1.Size = new System.Drawing.Size(242, 31);
+            this.rH_TextBox1.TabIndex = 7;
+            this.rH_TextBox1.Texts = "";
+            this.rH_TextBox1.UnderlinedStyle = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 335);
+            this.Controls.Add(this.rH_TextBox1);
+            this.Controls.Add(this.rH_DatePicker1);
+            this.Controls.Add(this.rH_CircularPictureBox1);
+            this.Controls.Add(this.rH_ComboBox1);
+            this.Controls.Add(this.rH_RadioButton1);
+            this.Controls.Add(this.rH_ProgressBar1);
+            this.Controls.Add(this.rH_ToggleButton1);
+            this.Controls.Add(this.rH_Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rH_CircularPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RH_Button rH_Button1;
+        private RH_ToggleButton rH_ToggleButton1;
+        private RH_ProgressBar rH_ProgressBar1;
+        private RH_RadioButton rH_RadioButton1;
+        private RH_ComboBox rH_ComboBox1;
+        private RH_CircularPictureBox rH_CircularPictureBox1;
+        private RH_DatePicker rH_DatePicker1;
+        private RH_TextBox rH_TextBox1;
+    }
+}
+
